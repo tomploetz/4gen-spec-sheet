d2364f300f1b5c644d71e89e2d9bf6cb0a533b7d
Tom <tom.ploetz1@gmail.com>
2022-05-03 14:31:16 -0600

    start

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,32 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "4GenSpecSheetTable": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "4genspecsheet78a1ea26": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string",
+            "CloudFrontDistributionID": "string",
+            "CloudFrontDomainName": "string",
+            "CloudFrontSecureURL": "string",
+            "CloudFrontOriginAccessIdentity": "string"
+        }
+    }
+}
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="text-center">
+    <v-col class="mb-4">
+      <v-text-field
+        label="Enter Part Number"
+        v-model="partNumber"
+      >
+      
+      </v-text-field>
+      <v-btn
+        @click="select"
+      >
+        Submit
+      </v-btn>
+    </v-col>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      partNumber: ""
+    }
+  },
+  methods: {
+    select () {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
